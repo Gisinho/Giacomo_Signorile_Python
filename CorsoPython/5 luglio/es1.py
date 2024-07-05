b8e14899cb94edad39b5072b41460c27a3488c6f
Gisinho <112932133+Gisinho@users.noreply.github.com>
2024-07-05 10:16:42 +0200

    aggiunti metodi

--- CorsoPython/5 luglio/es1.py
+++ CorsoPython/5 luglio/es1.py
@@ -3,7 +3,7 @@ la prima parte dell'esercizio riguarda solamente aggiunta e
 eliminazione cliente con il suo conto bancario e modifica cliente"""
 
 class Cliente:
-    def __init(self, nome, età,carta_credito,cvc):
+    def __init__ (self, nome, età,carta_credito,cvc):
 
         self.__nome=nome
         self.__età=età
@@ -19,7 +19,7 @@ class Cliente:
     def set_nome(self, nome):
         self.__nome = nome
 
-class GestioneBanca(Cliente):
+class GestoreBanca(Cliente):
     def __init__(self):
         self.__clienti = []
     
@@ -33,6 +33,80 @@ class GestioneBanca(Cliente):
                 print("Cliente rimosso.")
             else:
                 print("Cliente non registrato")
+    
+    def modifica_nome(self,nome,cvc,nuovo_nome):
+        trovato = False
+        for cliente in self.__clienti:
+            if nome == cliente.get_nome() and cvc == cliente.get_cvc():
+                cliente.set_nome(nuovo_nome)                
+                trovato=True
+        if trovato == True:
+            print("Hai modificato il nome")
+        else:
+            print("Utente non trovato")
+    
+    def lista_clienti(self):
+        for cliente in self.__clienti:
+            print(f"Lista clienti: {cliente.get_nome()}")
+        
+
+
+def menu():
+    print("""\n1. Apri conto
+2. Chiudi conto
+3. Modifica cliente
+4. Lista clienti
+0. Esci
+""")
+    
+
+banca=GestoreBanca()
+
+while True:
+    menu()
+    scelta=input("Inserisci la scelta: ")
+    if scelta=="1":
+        while True:
+            nome=input("Inserisci il nome del nuovo cliente: ")
+            if nome.isalpha():
+                break
+            else:
+                print("Inserisci solo lettere")
+        while True:
+            try:
+                eta=int(input("Inserisci l'età del cliente: "))
+                break
+            except:
+                print("ERRORE")
+        while True:
+            try:
+                carta_credito=int(input("Inserisci il numero della carta di credito: "))
+                break
+            except:
+                print("ERRORE")
+        while True:
+            cvc=input("Inserisci il cvc della carta: ")
+            if len(cvc)!=3:
+                print("cvc non conforme")
+            else:
+                break
+        nuovo_cliente=Cliente(nome, eta,carta_credito,cvc)
+        banca.aggiungi_cliente(nuovo_cliente)
+        print("Cliente aggiunto con successo.")
+    elif scelta=="2":       #RIMUOVI CLIENTE
+        nome=input("Inserisci il nome del cliente: ")
+        cvc=int(input("Inserisci il cvc della carta: "))
+        banca.rimuovi_cliente(nome,cvc)
+    elif scelta=="3":
+        nome=input("Inserisci il vecchio nome del cliente")
+        cvc=int(input("Inserisci il cvc della carta: "))
+        nuovo_nome=input("Inserisci il nuovo nome del cliente: ")
+        banca.modifica_nome(nome,cvc,nuovo_nome)
+    elif scelta == "4":
+        banca.lista_clienti()
+        break
+    elif scelta=="0":
+        break
 
 
     
