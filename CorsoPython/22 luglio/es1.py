36f88000e97550d338c86acf246bd1e8671bc13d
Gisinho <112932133+Gisinho@users.noreply.github.com>
2024-07-22 16:17:31 +0200

    aggiunte funzionalita

--- CorsoPython/22 luglio/es1.py
+++ CorsoPython/22 luglio/es1.py
@@ -15,3 +15,61 @@ Creare un grafico a linee del numero di visitatori giornalieri.
 Aggiungere al grafico la media mobile a 7 giorni per mostrare la
 tendenza settimanale.
 Creare un secondo grafico che mostri la media mensile dei visitatori."""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generazione dei Dati
+giorni = 365
+media_visitatori = 2000
+deviazione_standard = 500
+
+# Impostiamo un range che approssima la media e la deviazione standard
+min_visitatori = media_visitatori - 2 * deviazione_standard
+max_visitatori = media_visitatori + 2 * deviazione_standard
+
+# Generazione dei visitatori giornalieri
+visitatori = np.random.randint(min_visitatori, max_visitatori, size=giorni)
+
+#aumento graduale fino a 500visitatori in piu
+trend= np.linspace(0,500,giorni)
+
+#aggiungo trend crescente ai dati simulati
+visitatori= visitatori+trend
+print(visitatori)
+
+"""Creazione dataframe"""
+date = pd.date_range(start='2023-01-01', periods=giorni, freq='D')
+df = pd.DataFrame({'Date':date, 'Visitatori':visitatori})
+
+df.set_index('Date', inplace=True)#utilizzato per modificare il df e settare Date come indice
+print(df)
+
+
+media_mensile=df.resample('ME').mean()
+print("\nMedia mensile dei visitatori:\n", media_mensile)
+
+dev_mensile=df.resample('ME').std()
+print("\nDeviazione standard mensile:\n", dev_mensile)
+
+
+
+#Grafico a linee del numero di visitatori giornalieri
+plt.figure()
+plt.plot(df.index, df['Visitatori'], label='Visitatori giornalieri')
+plt.title('Numero di visitatori giornalieri')
+plt.xlabel('Date')
+plt.ylabel('Numero di visitatori')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# Grafico della media mensile dei visitatori
+plt.figure()
+plt.plot(media_mensile.index, media_mensile['Visitatori'])
+plt.title('Media mensile dei visitatori')
+plt.xlabel('Mese')
+plt.ylabel('Numero di visitatori')
+plt.show()
